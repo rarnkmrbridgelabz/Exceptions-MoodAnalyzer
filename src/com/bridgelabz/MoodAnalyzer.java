0370da46bdce7fe287cd9f43ce372ed9c9833f41
Arun <rarnkmrbridgelabz.com>
2022-04-13 16:57:03 +0530

    Adding UC-1.2

--- src/com/bridgelabz/MoodAnalyzer.java
+++ src/com/bridgelabz/MoodAnalyzer.java
@@ -4,30 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MoodAnalyzer {
+    public String message;
 
-        List<String> list = new ArrayList<>();
-        String sad1 = "Sad";
-        String sad2 = "sad";
-
-        public static void main(String[] args) {
-            MoodAnalyzer moodAnalyser = new MoodAnalyzer();
-            System.out.println("Welcome to MoodAnalyser:");
-            String str = "I am in any mood";
-            System.out.println(str);
-            System.out.println(moodAnalyser.analyseMood(str));
+    public MoodAnalyzer(String message){
+        this.message = message;
+    }
+    public  String analyseMood() {
+        if(this.message.toLowerCase().contains("sad")) {
+            return "sad";
         }
-
-        private String analyseMood(String str) {
-            String find = "sad";
-            String mood = null;
-            int i = str.indexOf(find);
-            if(i>0) {
-                mood = find;
-            }
-            else {
-                mood = "Happy";
-            }
-            return mood;
+        else {
+            return  "Happy";
         }
+    }
+
     }
 
